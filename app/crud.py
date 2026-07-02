bc17e6c6080103424bbcbcc202b2fb1d9b16c8d1
laele-hali <laelehali@gmail.com>
2026-07-02 11:17:15 +0100

    feat: add list cars endpoint

--- app/crud.py
+++ app/crud.py
@@ -2,9 +2,34 @@ from app.database import cars_collection
 from app.schemas import CarCreate
 
 
+def car_helper(car) -> dict:
+    return {
+        "id": str(car["_id"]),
+        "manufacturer": car["manufacturer"],
+        "model": car["model"],
+        "model_year": car["model_year"],
+        "performance_class": car["performance_class"],
+        "pi": car["pi"],
+        "rarity": car["rarity"],
+        "drivetrain": car["drivetrain"],
+        "is_new": car.get("is_new", False),
+        "nickname": car.get("nickname"),
+        "garage_slot": car.get("garage_slot"),
+    }
+
+
 async def create_car(car: CarCreate):
     car_dict = car.model_dump()
     result = await cars_collection.insert_one(car_dict)
 
-    car_dict["id"] = str(result.inserted_id)
-    return car_dict
+    created_car = await cars_collection.find_one({"_id": result.inserted_id})
+    return car_helper(created_car)
+
+
+async def get_all_cars():
+    cars = []
+
+    async for car in cars_collection.find():
+        cars.append(car_helper(car))
+
+    return cars
--- app/routers/cars.py
+++ app/routers/cars.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.crud import create_car
+from app.crud import create_car, get_all_cars
 from app.schemas import CarCreate, CarResponse
 
 router = APIRouter(
@@ -9,11 +9,12 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def get_cars():
-    return {"message": "List of cars will go here"}
+@router.get("/", response_model=list[CarResponse])
+async def get_cars():
+    return await get_all_cars()
 
 
 @router.post("/", response_model=CarResponse)
 async def add_car(car: CarCreate):
     return await create_car(car)
+
